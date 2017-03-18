e6c1b7872ffa1b37e34ead3ea86abf48b5c3433b
Vaillant Jeremy <vaillant.jeremy@dev-crea.com>
2017-03-18 16:41:09 +0100

    Replace test with rspec-rails

--- test/controllers/me_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test_helper'
-
-# Test for Me Controller
-class MeControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/models/link_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test_helper'
-
-# Test Links resource
-class LinkTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
