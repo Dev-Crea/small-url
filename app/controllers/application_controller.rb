dd37138c3b94ab599c180479b79d4048c28acad0
Vaillant Jeremy <vaillant.jeremy@dev-crea.com>
2017-03-18 17:00:02 +0100

    Increment click

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 # Core to Controllers
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def redirect_to_url
-    redirect_to Link.find_by(key: params[:key]).url
+    redirect_to Link.search_url(params[:key])
   end
 end
--- app/models/link.rb
+++ app/models/link.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # Core to model Links
 class Link
   include Mongoid::Document
@@ -7,4 +9,10 @@ class Link
   field :key,         type: String
   field :expire_in,   type: Time,     default: Time.zone.now + 24.hours
   field :click,       type: Integer,  default: 0
+
+  def self.search_url(token)
+    link = Link.find_by(key: token)
+    link.set(click: link.click + 1)
+    link.url
+  end
 end
